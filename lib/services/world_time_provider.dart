d258be0755dafb8a56a047267368b18ae8bd2705
Matias Ruiz <matiasruiz6899@gmail.com>
2023-07-02 23:01:09 -0300

    Favorites screen and current location

--- lib/services/world_time_provider.dart
+++ lib/services/world_time_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
 
@@ -6,14 +8,52 @@ class WorldTimeProvider extends ChangeNotifier {
       flag: 'argentina.png',
       location: 'Argentina',
       url: 'America/Argentina/Cordoba');
+  late WorldTime localTime;
+  final Future<List<WorldTime>> _worldTimes = generateWorldTimes();
+  final List<WorldTime> _favorites = [];
+  static bool _init = true;
+
+  Future<List<WorldTime>> get worldTimes => _worldTimes;
+  List<WorldTime> get favorites => _favorites;
 
   Future<WorldTime> getTime() async {
+    if (_init) {
+      _init = false;
+      localTime = await worldTime.getLocalTime();
+      return localTime;
+    }
+    await localTime.getTime();
     await worldTime.getTime();
     return worldTime;
   }
 
+  static Future<List<WorldTime>> generateWorldTimes() async {
+    List<String> list = await File('assets/country.csv').readAsLines();
+    List<List<String>> countries = list.map((e) => e.split(';')).toList();
+    List<WorldTime> worldTimes = countries
+        .map(
+          (e) => WorldTime(
+            flag: 'https://flagcdn.com/w160/${e[0].toLowerCase()}.png',
+            location: e[1],
+            url: e[2],
+          ),
+        )
+        .toList();
+    return worldTimes;
+  }
+
   void setWorldTime(WorldTime worldTime) {
     this.worldTime = worldTime;
+    notifyListeners();
+  }
+
+  void addToFavorites(WorldTime worldTime) {
+    _favorites.add(worldTime);
+    notifyListeners();
+  }
+
+  void removeFromFavorites(WorldTime worldTime) {
+    _favorites.remove(worldTime);
     notifyListeners();
   }
 }
